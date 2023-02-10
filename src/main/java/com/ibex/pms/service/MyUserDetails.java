bb8763f40fab1580fb4e75ad7eaaf65f5c522675
Luwam Abraham <luwam.yihdego@miu.edu>
2023-02-09 23:01:38 -0600

    User
    User issues

--- src/main/java/com/ibex/pms/service/MyUserDetails.java
+++ src/main/java/com/ibex/pms/service/MyUserDetails.java
@@ -1,15 +1,12 @@
 package com.ibex.pms.service;
 
 import com.ibex.pms.domain.Role;
-import com.ibex.pms.domain.User;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class MyUserDetails implements UserDetails {
     private String email;
